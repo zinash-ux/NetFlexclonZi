0939cf344e5dfd65d84e63ef3f803ca6553d11cb
zinash <[zinashado2017@gmail.com]>
2021-07-22 19:55:03 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import Row from './Row';
+import request from './requst';
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+      <Banner />
+      
+     <Row 
+     title="NETFLIX ORIGINALS"
+     fetchUrl={request.fetchNetflixOriginals}
+      isLargeRow
+      />
+     <Row title="Trending Now" 
+     fetchUrl={request.fetchTrending}
+     />
+     <Row title="" 
+     fetchUrl={request.fetchTopRatedMovies}
+     />
+     <Row title="Action Movies"
+     fetchUrl={request.fetchActionMovies}
+     />
+     <Row title="Commedy Movies"
+     fetchUrl={request.fetchComedyMovies}
+     />
+     <Row title="Horror Movies"
+     fetchUrl={request.fetchHorrorMovies}
+     />
+     <Row title="Romance Movies"
+     fetchUrl={request.fetchRomanceMovies}
+     />
+
     </div>
   );
 }
